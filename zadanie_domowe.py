2ea650a6c2f947f21dca36953cd30d07e2f6c625
kubabusz21 <97914082+kubabusz21@users.noreply.github.com>
2022-01-26 21:14:21 +0100

    Create zadanie_domowe.py

---
+++ zadanie_domowe.py
@@ -0,0 +1,22 @@
+Stan_autobusu = 'Pusty'
+
+print('Jak tam autobusik? -', Stan_autobusu)
+
+print()
+
+if Stan_autobusu == 'Przepelniony po brzegi':
+        print('Lepiej przejdz sie na piechote')
+
+if Stan_autobusu == 'Pusty':
+        print('Wsiadaj smialo!')
+
+if Stan_autobusu == 'Spoznil sie...':
+        print('Mam nadzieje ze nie spoznisz sie do pracy')
+
+elif Stan_autobusu == 'Stara Helga znowu jedzie':
+        print('Nawet nie mysl o tym zeby wsiadac')
+
+
+else:
+        print('Musisz samodzielnie zdecydowac co robic, mam nadzieje ze masz bilet!')
+
